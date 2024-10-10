9579e648d1b37f16d42500835c9b9f88c9c0e459
Lihan Yang <ly1423@vm-shell3.doc.ic.ac.uk>
2024-10-10 15:39:14 +0100

    used timer.c by LH

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -24,19 +24,34 @@ static int64_t ticks;
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
+/* Info for a sleeping thread. */
+static struct sleeper
+{
+  struct list_elem elem;
+  int64_t sleep_until;        /* Ticks until which this thread should sleep. */
+  struct semaphore sema;
+};
+
+/* List of sleepers. */
+static struct list sleepers;
+
 static intr_handler_func timer_interrupt;
+static bool thread_compare_sleep_ticks (const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 static void real_time_delay (int64_t num, int32_t denom);
 
 /* Sets up the timer to interrupt TIMER_FREQ times per second,
-   and registers the corresponding interrupt. */
+   registers the corresponding interrupt, and initialize the
+   list for sleeping threads. */
 void
 timer_init (void) 
 {
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
+
+  list_init (&sleepers);
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
@@ -89,11 +104,29 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  int64_t start = timer_ticks ();
-
+  enum intr_level old_level;
+  struct sleeper sleeper;
+  
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  
+  sleeper.sleep_until = ticks + timer_ticks ();
+
+  sema_init (&sleeper.sema, 0);
+
+  old_level = intr_disable ();
+  list_insert_ordered (&sleepers, &(sleeper.elem), &thread_compare_sleep_ticks, 0);
+  intr_set_level (old_level);
+
+  sema_down (&sleeper.sema);
+}
+
+/* Compares two threads list elements according to sleep_until. */
+static bool
+thread_compare_sleep_ticks(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED)
+{
+  struct sleeper *ta = list_entry (a, struct sleeper, elem);
+  struct sleeper *tb = list_entry (b, struct sleeper, elem);
+  return ta->sleep_until < tb->sleep_until;
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -166,11 +199,29 @@ timer_print_stats (void)
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
 
-/* Timer interrupt handler. */
+/* Timer interrupt handler. Wakes sleeping threads if needed. */
 static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
+  struct list_elem *e;
+
   ticks++;
+
+  while (!list_empty (&sleepers))
+  {
+    e = list_begin (&sleepers);
+    struct sleeper *sleeper = list_entry (e, struct sleeper, elem);
+    if (sleeper->sleep_until <= ticks)
+      {
+        list_pop_front (&sleepers);
+        sema_up (&sleeper->sema);
+      }
+    else
+      {
+        break;
+      }
+  }
+
   thread_tick ();
 }
 
