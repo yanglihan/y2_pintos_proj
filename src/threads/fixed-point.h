7b1ff7302d2f1ff751fed24c4ea82517d7b49b70
Henry Wu <hw2823@ic.ac.uk>
2024-10-17 15:53:57 +0100

    Debugged fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,23 +1,23 @@
 #define Q 14
 
-#define F (1 << Q)
+#define F ((1 << Q))
 
-#define TO_FP(n) (n * F)
+#define TO_FP(n) ((n) * (F))
 
-#define TO_INT_ROUND_DOWN(x) (x / F)
+#define TO_INT_ROUND_DOWN(x) ((x) / (F))
 
-#define TO_INT_ROUND_NEAREST(x) ((x > 0) ? (x + F / 2) / 2 : (x - F / 2) / 2)
+#define TO_INT_ROUND_NEAREST(x) ((x > 0) ? ((x) + (F) / 2) / 2 : ((x) - (F) / 2) / 2)
 
-#define ADD_FP_FP(x, y) (x + y)
+#define ADD_FP_FP(x, y) ((x) + (y))
 
-#define SUBTRACT_FP_FP(x, y) (x - y)
+#define SUBTRACT_FP_FP(x, y) ((x) - (y))
 
-#define ADD_FP_INT(x, n) (x + n * F)
+#define ADD_FP_INT(x, n) ((x) + (n) * (F))
 
-#define SUBTRACT_FP_INT(x, n) (x - n * F)
+#define SUBTRACT_FP_INT(x, n) ((x) - (n) * (F))
 
-#define MULTIPLY_FP_FP(x, y) (((int64_t) x) * y / F)
+#define MULTIPLY_FP_FP(x, y) (((int64_t) (x)) * (y) / (F))
 
-#define DIVIDE_FP_FP(x, y) (((int64_t) x) * F / y)
+#define DIVIDE_FP_FP(x, y) (((int64_t) (x)) * (F) / (y))
 
-#define DIVIDE_FP_INT(x, n) (x / n)
+#define DIVIDE_FP_INT(x, n) ((x) / (n))
